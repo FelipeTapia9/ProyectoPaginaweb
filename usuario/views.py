12f614365e2f070bb832645fe90c53c7a1a264bf
FelipeTapia <feli.tapias@alumnos.duoc.cl>
2021-01-24 21:11:57 -0300

    aplicacion semi lista

--- usuario/views.py
+++ usuario/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'usuario/inicio.html', {})
 
+def lista(request):
+    return render(request, 'usuario/lista.persona.html', {})
+
